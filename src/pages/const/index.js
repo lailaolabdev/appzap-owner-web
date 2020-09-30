0b3150f9f3e797c44e4eec578a8ab0a57920da29
khamla@siyivang <ksiyivang@gmail.com>
2020-09-30 16:24:46 +0700

    create-page-tables

---
+++ src/pages/const/index.js
@@ -0,0 +1,5 @@
+export const NAV = {
+    backgroundColor: '#F9F9F9',
+    marginTop: -10,
+    paddingTop: 10,
+}
--- src/pages/table/Table.js
+++ src/pages/table/Table.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React from "react"
+import useReactRouter from "use-react-router"
+import Nav from "react-bootstrap/Nav"
 
 const Table = () => {
+  const { history, location, match } = useReactRouter();
+  console.log("location: ", location)
+  console.log("match: ", match)
   return (
+
     <div>
-      <h1>Hello world</h1>
+      <Nav
+        variant="tabs"
+        style={NAV}
+      //   defaultActiveKey={
+      //     location.pathname.split("/")[3] == "tables"
+      //       ? `tables/`
+      // }
+      >
+        <Nav.Item>
+          <Nav.Link
+          // href={`/orders/${orderId}/orders_detail_post/pagenumber/${numberPage}`}
+          >
+            ໂຕະທັງໜົດ
+					</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
+
   );
 };
 
 export default Table;
+
+const NAV = {
+  backgroundColor: '#F9F9F9',
+  marginTop: -10,
+  paddingTop: 10,
+}
