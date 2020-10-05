f0c430d6d559c78eeb543545c5dc75bfcd4c5257
soulinmaikadua <soulinmaikadua@gmail.com>
2020-10-05 17:36:10 +0700

    latest commit

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,25 +1,26 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import useReactRouter from "use-react-router";
 import CustomNav from "./component/CustomNav";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import Checkbox from "@material-ui/core/Checkbox";
-import { getOrders } from '../../services/order'
-
+import { getOrders } from "../../services/order";
+import { getHeaders } from "../../services/auth";
 const Order = () => {
   const { history, location, match } = useReactRouter();
 
- const [orders,setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
   React.useEffect(() => {
     const fetchOrder = async () => {
       const res = await getOrders();
       setOrders(res);
-    }
+    };
     fetchOrder();
-  }, [])
-  if(orders){
+  }, []);
+  if (orders) {
     console.log("12345: ", orders);
   }
+  getHeaders();
   return (
     <div>
       <CustomNav default="/orders/pagenumber/1" />
@@ -27,10 +28,10 @@ const Order = () => {
         <Table responsive className="staff-table-list borderless table-hover">
           <thead style={{ backgroundColor: "#F1F1F1" }}>
             <tr>
-            <th style={{ width: 50 }}></th>
+              <th style={{ width: 50 }}></th>
               <th style={{ width: 100 }}>ລຳດັບ</th>
               <th style={{ width: 200 }}>ຊື່ເມນູ</th>
-              <th style={{ width: 100}}>ຈຳນວນ</th>
+              <th style={{ width: 100 }}>ຈຳນວນ</th>
               <th style={{ width: 100 }}>ເບີໂຕະ</th>
               <th style={{ width: 100 }}>ສະຖານະ</th>
               <th style={{ width: 100 }}></th>
@@ -38,25 +39,25 @@ const Order = () => {
             </tr>
           </thead>
           {food.map((value, index) => {
-                  return (
-                    <tr index={value}>
-                      <td >
-                        <Checkbox
-                          // hidden={isAdmin}
-                          color="primary"
-                          name="selectAll"
-                        // onChange={(e) => _checkAll(e)}
-                        />
-                      </td>
-                      <td>{index + 1}</td>
-                      <td>{value.menu}</td>
-                      <td>{value.quantity}</td>
-				  	          <td>{value.table}</td>
-                      <td>{value.status}</td>
-                      <td>{value.datetime}</td>
-                    </tr>
-                  )
-                })}
+            return (
+              <tr key={index}>
+                <td>
+                  <Checkbox
+                    // hidden={isAdmin}
+                    color="primary"
+                    name="selectAll"
+                    // onChange={(e) => _checkAll(e)}
+                  />
+                </td>
+                <td>{index + 1}</td>
+                <td>{value.menu}</td>
+                <td>{value.quantity}</td>
+                <td>{value.table}</td>
+                <td>{value.status}</td>
+                <td>{value.datetime}</td>
+              </tr>
+            );
+          })}
         </Table>
       </Container>
     </div>
@@ -66,10 +67,39 @@ const Order = () => {
 export default Order;
 
 const food = [
-	{ menu: "ຕົ້ມຊໍາກຸ້ງ", quantity: "3", table: 1 , status: "ລໍຖ້າ", datetime: "11-09-2020" },
-	{ menu: "ຕົ້ມຊໍາກຸ້ງ", quantity: "3", table: 2 , status: "ລໍຖ້າ", datetime: "11-09-2020" },
-	{ menu: "ຕົ້ມຊໍາກຸ້ງ", quantity: "3", table: 3 , status: "ລໍຖ້າ", datetime: "11-09-2020" },
-	{ menu: "ຕົ້ມຊໍາກຸ້ງ", quantity: "3", table: 4 , status: "ລໍຖ້າ", datetime: "11-09-2020" },
-	{ menu: "ຕົ້ມຊໍາກຸ້ງ", quantity: "3", table: 5 , status: "ລໍຖ້າ", datetime: "11-09-2020" },
-  
-  ]
+  {
+    menu: "ຕົ້ມຊໍາກຸ້ງ",
+    quantity: "3",
+    table: 1,
+    status: "ລໍຖ້າ",
+    datetime: "11-09-2020",
+  },
+  {
+    menu: "ຕົ້ມຊໍາກຸ້ງ",
+    quantity: "3",
+    table: 2,
+    status: "ລໍຖ້າ",
+    datetime: "11-09-2020",
+  },
+  {
+    menu: "ຕົ້ມຊໍາກຸ້ງ",
+    quantity: "3",
+    table: 3,
+    status: "ລໍຖ້າ",
+    datetime: "11-09-2020",
+  },
+  {
+    menu: "ຕົ້ມຊໍາກຸ້ງ",
+    quantity: "3",
+    table: 4,
+    status: "ລໍຖ້າ",
+    datetime: "11-09-2020",
+  },
+  {
+    menu: "ຕົ້ມຊໍາກຸ້ງ",
+    quantity: "3",
+    table: 5,
+    status: "ລໍຖ້າ",
+    datetime: "11-09-2020",
+  },
+];
