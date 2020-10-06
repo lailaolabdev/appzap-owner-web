fb5ac3f022686bc47e22859f0928f28a92f55501
soulinmaikadua <soulinmaikadua@gmail.com>
2020-10-06 22:53:34 +0700

    latest commit

--- src/services/order.js
+++ src/services/order.js
@@ -1,18 +1,19 @@
-import axios from 'axios'
-import { END_POINT } from '../constants'
-import { getHeaders } from './auth';
-
+import axios from "axios";
+import { END_POINT } from "../constants";
+import { getHeaders } from "./auth";
 
 export const getOrders = async () => {
-    try {
-        const orders = await axios.get(`${END_POINT}/orders?status=CART`,{headers: getHeaders(),});
-        if (orders) {
-            let data = orders?.data;
-            return data;
-        } else {
-            return null;
-        }
-    } catch (error) {
-        console.log("get orders error:", error)
+  try {
+    const orders = await axios.get(`${END_POINT}/orders?status=CART`, {
+      headers: getHeaders(),
+    });
+    if (orders) {
+      let data = orders?.data;
+      return data;
+    } else {
+      return null;
     }
-}
+  } catch (error) {
+    console.log("get orders error:", error);
+  }
+};
--- src/services/table.js
+++ src/services/table.js
@@ -1,57 +1,57 @@
-import axios from 'axios'
-import { END_POINT } from '../constants'
-import { getHeaders } from './auth';
-
+import axios from "axios";
+import { END_POINT } from "../constants";
+import { getHeaders } from "./auth";
 
 export const tables = async () => {
-    try {
-        const tables = await axios.get(`${END_POINT}/tables`);
-        if (tables) {
-            let data = tables?.data;
-            return data;
-        } else {
-            return null;
-        }
-
-    } catch (error) {
-        console.log("get tables error:", error)
+  try {
+    const tables = await axios.get(`${END_POINT}/tables`);
+    if (tables) {
+      let data = tables?.data;
+      return data;
+    } else {
+      return null;
     }
-}
+  } catch (error) {
+    console.log("get tables error:", error);
+  }
+};
 
 // TO DO: get generatedCode
 
 export const generatedCode = async (data) => {
-    try {
-        const geneartedCode = await axios.post(`${END_POINT}/generates`, { table_id: data });
-        if (geneartedCode) {
-            let data = geneartedCode.data;
-            console.log("dataa:", data.code)
-            return data.code
-        } else {
-            return null
-        }
-    } catch (error) {
-        console.log("get genearted code error:", error)
+  try {
+    const geneartedCode = await axios.post(`${END_POINT}/generates`, {
+      table_id: data,
+    });
+    if (geneartedCode) {
+      let data = geneartedCode.data;
+      console.log("dataa:", data.code);
+      return data.code;
+    } else {
+      return null;
     }
-}
+  } catch (error) {
+    console.log("get genearted code error:", error);
+  }
+};
 
 // TO DO: get Order
 
 export const getOrderData = async () => {
-    try {
-        const orderData = await axios.get(`${END_POINT}/orders?status=CART`, { headers: getHeaders() })
-        if (orderData) {
-            let data = orderData?.data
-            return data
-        } else {
-            return null;
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const orderData = await axios.get(`${END_POINT}/orders?status=CART`, {
+      headers: getHeaders(),
+    });
+    if (orderData) {
+      let data = orderData?.data;
+      return data;
+    } else {
+      return null;
     }
-}
-
-
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // openTable
 
