e5c011dd223634f21ec1df40f10aa455c3cb39a5
soulinmaikadua <soulinmaikadua@gmail.com>
2020-10-16 14:49:57 +0700

    latest commit

---
+++ src/pages/qrcode/Qrcode.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { QRCode } from "react-qrcode-logo";
+const Qrcode = () => {
+  return (
+    <div>
+      <QRCode value="https://github.com/gcoro/react-qrcode-logo" />
+    </div>
+  );
+};
+
+export default Qrcode;
