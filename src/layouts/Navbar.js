d9e735d8393841033f36a21877ab66ca4a6c6c13
Joy xaiyavong <29xaiyavong@gmail.com>
2021-05-13 01:05:48 +0700

    add new message

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -8,16 +8,24 @@ import NavDropdown from "react-bootstrap/NavDropdown"
 import { USER_KEY, END_POINT, URL_PHOTO_AW3 } from "../constants"
 import useReactRouter from "use-react-router"
 import ImageProfile from "../image/profile.png"
-import { Badge } from 'react-bootstrap'
+import { Badge, Modal, Button, Table } from 'react-bootstrap'
 import socketIOClient from "socket.io-client";
+import moment from 'moment';
+import axios from 'axios';
+
 
 export default function NavBar() {
   const { history, location, match } = useReactRouter()
   const [userData, setUserData] = useState({})
   const socket = socketIOClient(END_POINT);
   const [NewChackBill, setNewChackBill] = useState()
+  const [getmassege, setgetmassege] = useState()
+
   socket.on("notificationCheckout", data => {
     setNewChackBill(data)
+  });
+  socket.on("messageAdmin", data => {
+    setgetmassege(data)
   });
   useEffect(() => {
     const ADMIN = localStorage.getItem(USER_KEY)
@@ -44,9 +52,43 @@ export default function NavBar() {
         setcheckBill(response)
       })
   }
+  const [messageData, setmessageData] = useState()
+  useEffect(() => {
+    _message()
+  }, [getmassege])
+  const _message = async () => {
+    const url = END_POINT + `/messages/?status=NOT`;
+    const _data = await fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        setmessageData(response)
+      })
+  }
   const _gotohistoryCheckbill = () => {
     history.push('/checkBill')
   }
+  const _updateMessage = async () => {
+    let getId = []
+    for (let p = 0; p < messageData?.length; p++) {
+      getId.push(messageData[p]?._id)
+    }
+    // ======> update ststus= yes message
+    const resData = await axios({
+      method: 'PUT',
+      url: END_POINT + `/messagesUpdateMany`,
+      data: {
+        getId
+      },
+    }).then(async function (response) {
+      if (response) {
+        window.location.reload();
+      }
+    }).catch(function (error) {
+    })
+  }
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div>
       <Navbar
@@ -68,6 +110,15 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" />
+          <Image
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM_EuMtQtLai1XobOTisIwqDSJLsyAsAzD4fu4RsNabketghLEL8iA2WIzqT0mrnHcVdU&usqp=CAU"
+            width={35}
+            height={35}
+            roundedCircle
+            onClick={handleShow}
+          />
+          <Badge variant="danger">{messageData?.length ? messageData?.length : ""}</Badge>
+          <div style={{ marginLeft: 20 }}></div>
           <Image
             src="https://icons-for-free.com/iconfiles/png/512/notification-131964743693202280.png"
             width={35}
@@ -91,13 +142,6 @@ export default function NavBar() {
                   : ""}
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                {/* <Dropdown.Item
-                  style={{ color: "#909090" }}
-                  onClick={() => _onDetailProfile()}
-                >
-                  Profile
-								</Dropdown.Item> */}
-                {/* <NavDropdown.Divider /> */}
                 <Dropdown.Item
                   style={{ color: "#909090" }}
                   onClick={() => _onLogout()}
@@ -115,6 +159,50 @@ export default function NavBar() {
           </Form>
         </Navbar.Collapse>
       </Navbar>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>ຂໍ້ຄວາມຈາກລູກຄ້າ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>ຊື່</th>
+                <th>ລະຫັດຕູບ</th>
+                <th>ຂໍ້ຄວາມ</th>
+                <th>ເວລາ</th>
+              </tr>
+            </thead>
+            <tbody>
+              {messageData?.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{item?.customer_nickname}</td>
+                    <td>{item?.code}</td>
+                    <td>{item?.text}</td>
+                    <td>{moment(item?.createdAt).format("HH:mm a")}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            ປິດ
+          </Button>
+          <Button variant="success" onClick={() => _updateMessage()}>
+            ອ່ານແລ້ວ
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
