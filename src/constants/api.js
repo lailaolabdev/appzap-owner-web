70754811835957075d8754f36e3affa69192bb05
Laosdev Dev <laosdev2022@gmail.com>
2021-11-09 16:41:58 +0700

    kokoko

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 import { USER_KEY } from './index'
-export const END_POINT_SEVER = "http://localhost:7070";
+export const END_POINT_SEVER = "http://localhost:7070"; //dev
 // export const END_POINT_SEVER = "http://13.229.122.188:7070";
 export const getLocalData = async () => {
     const _local = await localStorage.getItem(USER_KEY);
