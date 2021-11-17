c580bdf4af126e6ce649d542d8f68bd6935d44c7
TOxxTOxxAir <tovannapha3@gmail.com>
2021-11-17 23:36:44 +0700

    global state clean 1

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,48 +1,32 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, } from "react";
 import useReactRouter from "use-react-router";
 import Container from "react-bootstrap/Container";
-import { Table, Button, Nav, ButtonGroup, Form, Col } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 import { Checkbox, FormControlLabel } from "@material-ui/core";
 import moment from "moment";
-import Swal from 'sweetalert2'
 
 /**
  * import components
  */
-import UpdateModal from "./component/UpdateModal";
 import OrderNavbar from "./component/OrderNavbar";
-import CancelModal from "./component/CancelModal";
 
 /**
  * import function
  */
-import { updateOrderItem } from "../../services/order";
 import { orderStatus } from "../../helpers";
-import { CANCEL_STATUS, DOING_STATUS, SERVE_STATUS, END_POINT, USER_KEY, TITLE_HEADER, WAITING_STATUS } from "../../constants";
-import { SocketContext } from '../../services/socket';
+import { CANCEL_STATUS, DOING_STATUS, SERVE_STATUS, WAITING_STATUS } from "../../constants";
 import { useStore } from "../../store";
 
 const Order = () => {
   const { match } = useReactRouter();
 
   const { 
-    userData, 
     orderItems,
     getOrderItemsStore, 
+    handleCheckbox,
+    checkAllOrders,
+    handleUpdateOrderStatus
   } = useStore();
-
-  const [checkedToUpdate, setCheckedToUpdate] = useState([]);
-  const [cancelModal, setCancelModal] = useState(false);
-  const [updateModal, setUpdateModal] = useState(false);
-  // const [userData, setUserData] = useState({})
-  const [isLoading, setIsLoading] = useState(false);
-  // const [orderItems, setOrderItems] = useState()
-
-  /**
-  * useContext
-  */
-  const socket = useContext(SocketContext);
-
 
   /**
    * Initial Component
@@ -51,121 +35,17 @@ const Order = () => {
     getOrderItemsStore(WAITING_STATUS)
   }, [])
 
-
-
-  /**
-   * Subscribe Order Event
-   */
-  // useEffect(() => {
-  //   socket.on(`ORDER:${userData?.data?.storeId}`, (data) => {
-  //     setOrderItems(data)
-  //   });
-  //   return () => {
-  //     socket.off(`ORDER:${userData?.data?.storeId}`, () => {
-  //       console.log(`BYE BYE ORDER:${match?.params?.storeId}`)
-  //     });
-  //   };
-  // }, [socket, userData])
-
-
-  // const getData = async () => {
-  //   await setIsLoading(true);
-  //   await fetch(END_POINT + `/orderItems?status=WAITING&&storeId=${match?.params?.id}`, {
-  //     method: "GET",
-  //   }).then(response => response.json())
-  //     .then(json => setOrderItems(json));
-  //   await setIsLoading(false);
-  // }
-
-
-  const _handleUpdateOrderStatus = async (status) => {
-    let _updateItems = orderItems.filter((item) => item.isChecked).map((i) => {
-      return {
-        ...i,
-        status,
-        id: i._id
-      }
-    })
-    let _resOrderUpdate = await updateOrderItem(_updateItems,match?.params?.id);
-    if(_resOrderUpdate?.data?.message=="UPADTE_ORDER_ITEM_SECCESS"){
-      let _newOrderItem = orderItems.filter((item) => !item.isChecked);
-      // setOrderItems(_newOrderItem)
-      Swal.fire({
-       icon: 'success',
-       title: "ອັບເດດສະຖານະອໍເດີສໍາເລັດ",
-       showConfirmButton: false,
-       timer: 10000
-     })
-   }
-  };
-
-
-  const _handleCheckbox = async (order) => {
-    let _orderItems = [...orderItems]
-    let _newOrderItems = _orderItems.map((item) => {
-      if (item._id == order._id) {
-        return {
-          ...item,
-          isChecked: !item.isChecked
-        }
-      } else return item
-    })
-
-    // setOrderItems(_newOrderItems)
-  };
-
-
-
-
-  /**
-   * ພິມບິນ
-   */
-  const _prinbill = async () => {
-    let billId = []
-    for (let i = 0; i < checkedToUpdate?.length; i++) {
-      billId.push(checkedToUpdate[i]?.id)
-    }
-    await window.open(`/BillForChef/?id=${billId}`);
-    // _handleUpdate()
-  }
-
-  /**
-   * ເລືອກທຸກອັນ
-   */
-  const _checkAll = (item) => {
-    let _orderItems = [...orderItems]
-    let _newOrderItems;
-    if (item?.target?.checked) {
-      _newOrderItems = _orderItems.map((item) => {
-        return {
-          ...item,
-          isChecked: true
-        }
-      })
-    } else {
-      _newOrderItems = _orderItems.map((item) => {
-        return {
-          ...item,
-          isChecked: false
-        }
-      }) 
-    }
-    // setOrderItems(_newOrderItems)
-  }
-
-
-
   return (
     <div style={{}}>
       <OrderNavbar />
       <div style={{ flexDirection: 'row', justifyContent: "space-between", display: "flex", paddingTop: 15, paddingLeft: 15, paddingRight: 15 }}>
         <div style={{ alignItems: "end", flexDirection: 'column', display: "flex", justifyContent: "center" }}>
-          <FormControlLabel control={<Checkbox name="checkedC" onChange={(e) => _checkAll(e)} />} label={<div style={{ fontFamily: "NotoSansLao", fontWeight: "bold" }}>ເລືອກທັງໝົດ</div>} />
+          <FormControlLabel control={<Checkbox name="checkedC" onChange={(e) => checkAllOrders(e)} />} label={<div style={{ fontFamily: "NotoSansLao", fontWeight: "bold" }}>ເລືອກທັງໝົດ</div>} />
         </div>
         <div>
-          <Button variant="outline-warning" style={{ marginRight: 15, border: "solid 1px #FB6E3B", color: "#FB6E3B", fontWeight: "bold" }} onClick={() => _handleUpdateOrderStatus(CANCEL_STATUS)}>ຍົກເລີກ</Button>
-          <Button variant="light" style={{ marginRight: 15, backgroundColor: "#FB6E3B", color: "#ffffff", fontWeight: "bold" }} onClick={() => _handleUpdateOrderStatus(DOING_STATUS)}>ສົ່ງໄປຄົວ</Button>
-          <Button variant="light" style={{ backgroundColor: "#FB6E3B", color: "#ffffff", fontWeight: "bold" }} onClick={() => _handleUpdateOrderStatus(SERVE_STATUS)}>ເສີບແລ້ວ</Button>
+          <Button variant="outline-warning" style={{ marginRight: 15, border: "solid 1px #FB6E3B", color: "#FB6E3B", fontWeight: "bold" }} onClick={() => handleUpdateOrderStatus(CANCEL_STATUS,match?.params?.id)}>ຍົກເລີກ</Button>
+          <Button variant="light" style={{ marginRight: 15, backgroundColor: "#FB6E3B", color: "#ffffff", fontWeight: "bold" }} onClick={() => handleUpdateOrderStatus(DOING_STATUS,match?.params?.id)}>ສົ່ງໄປຄົວ</Button>
+          <Button variant="light" style={{ backgroundColor: "#FB6E3B", color: "#ffffff", fontWeight: "bold" }} onClick={() => handleUpdateOrderStatus(SERVE_STATUS,match?.params?.id)}>ເສີບແລ້ວ</Button>
         </div>
       </div>
       <Container fluid className="mt-3">
@@ -192,7 +72,7 @@ const Order = () => {
                       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: 50 }}>
                         <Checkbox
                           checked={order?.isChecked ? true : false}
-                          onChange={(e) => _handleCheckbox(order)}
+                          onChange={(e) => handleCheckbox(order)}
                           color="primary"
                           inputProps={{ "aria-label": "secondary checkbox" }}
                         />
@@ -218,16 +98,6 @@ const Order = () => {
           </tbody>
         </Table>
       </Container>
-      {/* <CancelModal
-        show={cancelModal}
-        hide={() => setCancelModal(false)}
-        handleCancel={_handleCancel}
-      />
-      <UpdateModal
-        show={updateModal}
-        hide={() => setUpdateModal(false)}
-        handleUpdate={_handleUpdate}
-      /> */}
     </div>
   );
 };
--- src/store/globalState/orderState.js
+++ src/store/globalState/orderState.js
@@ -1,6 +1,8 @@
 import { useState, useMemo, useCallback, useContext, useEffect } from "react";
+import Swal from 'sweetalert2'
 import { END_POINT } from "../../constants";
 import { getLocalData } from "../../constants/api";
+import { updateOrderItem } from "../../services/order";
 import { socket } from '../../services/socket'
 
 export const useOrderState = () => {
@@ -34,11 +36,76 @@ export const useOrderState = () => {
         await setIsLoading(false);
     }
 
+
+  const handleUpdateOrderStatus = async (status,storeId) => {
+    let _updateItems = orderItems.filter((item) => item.isChecked).map((i) => {
+      return {
+        ...i,
+        status,
+        id: i._id
+      }
+    })
+    let _resOrderUpdate = await updateOrderItem(_updateItems,storeId);
+    if(_resOrderUpdate?.data?.message=="UPADTE_ORDER_ITEM_SECCESS"){
+      let _newOrderItem = orderItems.filter((item) => !item.isChecked);
+      setOrderItems(_newOrderItem)
+      Swal.fire({
+       icon: 'success',
+       title: "ອັບເດດສະຖານະອໍເດີສໍາເລັດ",
+       showConfirmButton: false,
+       timer: 10000
+     })
+   }
+  };
+
+
+  const handleCheckbox = async (order) => {
+    let _orderItems = [...orderItems]
+    let _newOrderItems = _orderItems.map((item) => {
+      if (item._id == order._id) {
+        return {
+          ...item,
+          isChecked: !item.isChecked
+        }
+      } else return item
+    })
+
+    setOrderItems(_newOrderItems)
+  };
+
+
+  /**
+   * ເລືອກທຸກອໍເດີ
+   */
+   const checkAllOrders = (item) => {
+    let _orderItems = [...orderItems]
+    let _newOrderItems;
+    if (item?.target?.checked) {
+      _newOrderItems = _orderItems.map((item) => {
+        return {
+          ...item,
+          isChecked: true
+        }
+      })
+    } else {
+      _newOrderItems = _orderItems.map((item) => {
+        return {
+          ...item,
+          isChecked: false
+        }
+      }) 
+    }
+    // setOrderItems(_newOrderItems)
+  }
+
     return {
         isLoading,
         userData,
         orderItems,
         getOrderItemsStore,
+        handleUpdateOrderStatus,
+        handleCheckbox,
+        checkAllOrders,
         initialOrderSocket
     };
 };
