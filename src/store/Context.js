e8e8eabf9a9294a64f60d747f5e8489d345efcc5
TOxxTOxxAir <tovannapha3@gmail.com>
2021-11-17 17:00:51 +0700

    global state

--- src/store/Context.js
+++ src/store/Context.js
@@ -2,17 +2,6 @@ import React, { Dispatch, SetStateAction } from "react";
 
 export const Context = React.createContext({
   loading: false,
-  recipeList: null,
   tableData:null,
-  searchByName: () => {},
-  setRecipeList: () => {},
-  reviewLoading: false,
-  reviewBarOpen: false,
-  getRecipeById: () => {},
-  reviewedRecipe: null,
-  getRandomRecipes: () => {},
-  resetReviewState: () => {},
-  searchByCountry: () => {},
-  searchByCategory: () => {},
-  searchByIngredients: () => {},
+  
 });
--- src/store/StateProvider.js
+++ src/store/StateProvider.js
@@ -2,10 +2,8 @@ import React from "react";
 import { Context } from "./Context";
 import { useTableState } from "./globalState/tableState";
 import { useOrderState } from "./globalState/orderState";
-import { useGlobalState } from "./useGlobalState";
 
 export const StateProvider = ({ children }) => {
-    // const global = useGlobalState();
     const order = useOrderState();
     const table = useTableState();
     let store = Object.assign(order, table);
