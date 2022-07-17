13a51a6c9a7e4ce2d2c52f984ea2ee35726f1b67
TOxxTOxx <vannapha.s@lailaolab.com>
2022-07-17 20:21:25 +0700

    add endpoint2

--- src/pages/historiesUse/HistoryUse.js
+++ src/pages/historiesUse/HistoryUse.js
@@ -13,20 +13,42 @@ export default function HistoryUse() {
     const { history, location, match } = useReactRouter()
     const [data, setData] = useState()
     const [filtterModele, setFiltterModele] = useState("checkBill")
-    
+
+    const _page = match?.params?.page;
+    const LIMIT_PAGE = 300;
+    const [pageNumber, setPageNumber] = useState(_page ?? 1);
+    const [pageCountNumber, setPageCountNumber] = useState(10000);
+
+
 
     useEffect(() => {
         _getdataHistories();
     }, [])
     useEffect(() => {
         _getdataHistories();
+    }, [pageNumber])
+
+
+    useEffect(() => {
+        setPageNumber(1);
+        _getdataHistories();
     }, [filtterModele])
     const _getdataHistories = async () => {
-        await fetch(END_POINT_SEVER + `/v3/logs/skip/0/limit/500?storeId=${match?.params?.id}&modele=${filtterModele}`, {
+        await fetch(END_POINT_SEVER + `/v3/logs/skip/${pageNumber - 1}/limit/${LIMIT_PAGE}?storeId=${match?.params?.id}&modele=${filtterModele}`, {
             method: "GET",
         }).then(response => response.json())
             .then(json => setData(json));
     }
+
+    const onNextPage = () => {
+        setPageNumber(parseInt(pageNumber) != parseInt(pageCountNumber) ? parseInt(pageNumber) + 1 : parseInt(pageCountNumber))
+    }
+
+    const onBackPage = () => {
+        setPageNumber(parseInt(pageNumber) != 1 ? parseInt(pageNumber) - 1 : 1)
+    }
+
+
     return (
         <div style={TITLE_HEADER}>
             <div className="col-sm-12">
@@ -42,6 +64,9 @@ export default function HistoryUse() {
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link eventKey="/resetBill" onClick={() => setFiltterModele("resetBill")}>ແກ້ໄຂບີນ</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="/transferTable" onClick={() => setFiltterModele("transferTable")}>ຍ້າຍລວມໂຕະ</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </div>
@@ -60,9 +85,9 @@ export default function HistoryUse() {
                         {data?.map((item, index) => {
                             return (
                                 <tr>
-                                    <td>{index + 1}</td>
+                                    <td>{((pageNumber-1) * LIMIT_PAGE) + index + 1}</td>
                                     <td>{item?.user}</td>
-                                    <td style={{ color: item?.event==="INFO" ? "green":"red"}}>{item?.event}</td>
+                                    <td style={{ color: item?.event === "INFO" ? "green" : "red" }}>{item?.event}</td>
                                     <td>{item?.eventDetail}</td>
                                     <td>{moment(item?.createdAt).format("DD/MM/YYYY HH:mm a")}</td>
                                 </tr>
@@ -70,6 +95,15 @@ export default function HistoryUse() {
                         })}
                     </tbody>
                 </table>
+
+                {pageCountNumber &&
+                    <div className="row col-12 justify-content-center" style={{ marginBottom: 24 }}>
+                        <p className="col-1" style={{ color: 'blue', cursor: 'pointer' }} onClick={() => onBackPage()}><u>ກັບຄືນ  </u></p>
+                        <p className="col-4 text-center">{" "}  ຫນ້າ {pageNumber} / {pageCountNumber}  {" "}</p>
+                        <p className="col-1" style={{ color: 'blue', cursor: 'pointer' }} onClick={() => onNextPage()}><u>  ຫນ້າຕໍ່ໄປ</u></p>
+                    </div>
+                }
+
             </div>
         </div>
     )
