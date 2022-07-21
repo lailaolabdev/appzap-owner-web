7fabc30644b94a213bfdc0a5ceec726678fd6828
Phoudthavisack <66727427+Phoudthavisack@users.noreply.github.com>
2022-07-21 17:45:04 +0700

    okey

--- src/pages/historiesUse/HistoryUse.js
+++ src/pages/historiesUse/HistoryUse.js
@@ -55,63 +55,160 @@ export default function HistoryUse() {
 
 
     return (
-        <div style={{}}>
-            {isLoading ? <AnimationLoading /> : <div />}
-            <div className="col-sm-12">
-
-                <Nav fill variant="tabs" defaultActiveKey="/home" style={{ fontWeight: "bold", backgroundColor: "#f8f8f8", border: "none", height: 60 }}>
-                    <Nav.Item>
-                        <Nav.Link eventKey="/canceled" style={{ color: "#FB6E3B", border: "none", height: 60, display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() => setFiltterModele("checkBill")}> <FontAwesomeIcon icon={faTable} ></FontAwesomeIcon> <div style={{ width: 8 }}></div> ຄິດໄລ່ເງິນ</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="/finance" style={{ color: "#FB6E3B", border: "none", height: 60, display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() => setFiltterModele("canceled")}><FontAwesomeIcon icon={faCoins} ></FontAwesomeIcon> <div style={{ width: 8 }}></div>  ຍົກເລີກອາຫານ</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="/print" style={{ color: "#FB6E3B", border: "none", height: 60, display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() => setFiltterModele("print")}><FontAwesomeIcon icon={faPrint} ></FontAwesomeIcon> <div style={{ width: 8 }}></div>  ປີນເຕີ</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="/resetBill" style={{ color: "#FB6E3B", border: "none", height: 60, display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() => setFiltterModele("resetBill")}><FontAwesomeIcon icon={faCertificate} ></FontAwesomeIcon> <div style={{ width: 8 }}></div>  ແກ້ໄຂບີນ</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="/transferTable" style={{ color: "#FB6E3B", border: "none", height: 60, display: "flex", justifyContent: "center", alignItems: "center" }} onClick={() => setFiltterModele("transferTable")}><FontAwesomeIcon icon={faPeopleArrows} ></FontAwesomeIcon> <div style={{ width: 8 }}></div>  ຍ້າຍລວມໂຕະ</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </div>
-            <div className="col-sm-12">
-                <table className="table table-hover">
-                    <thead className="thead-light">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">ຊື່ຜູ້ຈັດການ</th>
-                            <th scope="col">ສະຖານະ</th>
-                            <th scope="col">ລາຍລະອຽດ</th>
-                            <th scope="col">ວັນເວລາ</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data?.map((item, index) => {
-                            return (
-                                <tr>
-                                    <td>{((pageNumber - 1) * LIMIT_PAGE) + index + 1}</td>
-                                    <td>{item?.user}</td>
-                                    <td style={{ color: item?.event === "INFO" ? "green" : "red" }}>{item?.event}</td>
-                                    <td>{item?.eventDetail}</td>
-                                    <td>{moment(item?.createdAt).format("DD/MM/YYYY HH:mm a")}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-
-                {pageCountNumber &&
-                    <div className="row col-12 justify-content-center" style={{ marginBottom: 24 }}>
-                        <p className="col-1" style={{ color: 'blue', cursor: 'pointer' }} onClick={() => onBackPage()}><u>ກັບຄືນ  </u></p>
-                        <p className="col-4 text-center">{" "}  ຫນ້າ {pageNumber} / {pageCountNumber}  {" "}</p>
-                        <p className="col-1" style={{ color: 'blue', cursor: 'pointer' }} onClick={() => onNextPage()}><u>  ຫນ້າຕໍ່ໄປ</u></p>
-                    </div>
-                }
-
-            </div>
+      <div style={{}}>
+        {isLoading ? <AnimationLoading /> : <div />}
+        <div className='col-sm-12'>
+          <Nav
+            fill
+            variant='tabs'
+            defaultActiveKey='/home'
+            style={{
+              fontWeight: "bold",
+              backgroundColor: "#f8f8f8",
+              border: "none",
+              height: 60,
+            }}>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='/canceled'
+                style={{
+                  color: "#FB6E3B",
+                  border: "none",
+                  height: 60,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onClick={() => setFiltterModele("checkBill")}>
+                {" "}
+                <FontAwesomeIcon icon={faTable}></FontAwesomeIcon>{" "}
+                <div style={{ width: 8 }}></div> ຄິດໄລ່ເງິນ
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='/finance'
+                style={{
+                  color: "#FB6E3B",
+                  border: "none",
+                  height: 60,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onClick={() => setFiltterModele("canceled")}>
+                <FontAwesomeIcon icon={faCoins}></FontAwesomeIcon>{" "}
+                <div style={{ width: 8 }}></div> ຍົກເລີກອາຫານ
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='/print'
+                style={{
+                  color: "#FB6E3B",
+                  border: "none",
+                  height: 60,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onClick={() => setFiltterModele("print")}>
+                <FontAwesomeIcon icon={faPrint}></FontAwesomeIcon>{" "}
+                <div style={{ width: 8 }}></div> ປີນເຕີ
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='/resetBill'
+                style={{
+                  color: "#FB6E3B",
+                  border: "none",
+                  height: 60,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onClick={() => setFiltterModele("resetBill")}>
+                <FontAwesomeIcon icon={faCertificate}></FontAwesomeIcon>{" "}
+                <div style={{ width: 8 }}></div> ແກ້ໄຂບີນ
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='/transferTable'
+                style={{
+                  color: "#FB6E3B",
+                  border: "none",
+                  height: 60,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onClick={() => setFiltterModele("transferTable")}>
+                <FontAwesomeIcon icon={faPeopleArrows}></FontAwesomeIcon>{" "}
+                <div style={{ width: 8 }}></div> ຍ້າຍລວມໂຕະ
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
         </div>
-    )
+        <div className='col-sm-12'>
+          <table className='table table-hover'>
+            <thead className='thead-light'>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>ຊື່ຜູ້ຈັດການ</th>
+                <th scope='col'>ສະຖານະ</th>
+                <th scope='col'>ລາຍລະອຽດ</th>
+                <th scope='col'>ເຫດຜົນ</th>
+                <th scope='col'>ວັນເວລາ</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{(pageNumber - 1) * LIMIT_PAGE + index + 1}</td>
+                    <td>{item?.user}</td>
+                    <td
+                      style={{
+                        color: item?.event === "INFO" ? "green" : "red",
+                      }}>
+                      {item?.event}
+                    </td>
+                    <td>{item?.eventDetail}</td>
+                    <td>{item?.reason || "-"}</td>
+                    <td>
+                      {moment(item?.createdAt).format("DD/MM/YYYY HH:mm a")}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+          {pageCountNumber && (
+            <div
+              className='row col-12 justify-content-center'
+              style={{ marginBottom: 24 }}>
+              <p
+                className='col-1'
+                style={{ color: "blue", cursor: "pointer" }}
+                onClick={() => onBackPage()}>
+                <u>ກັບຄືນ </u>
+              </p>
+              <p className='col-4 text-center'>
+                {" "}
+                ຫນ້າ {pageNumber} / {pageCountNumber}{" "}
+              </p>
+              <p
+                className='col-1'
+                style={{ color: "blue", cursor: "pointer" }}
+                onClick={() => onNextPage()}>
+                <u> ຫນ້າຕໍ່ໄປ</u>
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
 }
