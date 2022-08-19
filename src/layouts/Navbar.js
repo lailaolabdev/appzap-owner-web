8f1340cea3dfee65e4554ae25f724bdd02f751cf
phoudthavisack lsp <phoudthavisack@MacBook-Pro-khxng-phoudthavisack.local>
2022-08-20 03:00:20 +0700

    dev v4.4.2

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,49 +1,49 @@
-import React, { useState, useEffect, useContext } from "react"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
-import Form from "react-bootstrap/Form"
-import Image from "react-bootstrap/Image"
-import Dropdown from "react-bootstrap/Dropdown"
-import { USER_KEY, END_POINT, URL_PHOTO_AW3,VERSION } from "../constants"
-import useReactRouter from "use-react-router"
-import ImageProfile from "../image/profile.png"
-import { Badge, Modal, Button, Table } from 'react-bootstrap'
-import moment from 'moment';
-import axios from 'axios';
-import { SocketContext } from "../services/socket"
-import { socket } from '../services/socket'
-
+import React, { useState, useEffect, useContext } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+import Dropdown from "react-bootstrap/Dropdown";
+import { USER_KEY, END_POINT, URL_PHOTO_AW3, VERSION } from "../constants";
+import useReactRouter from "use-react-router";
+import ImageProfile from "../image/profile.png";
+import { Badge, Modal, Button, Table } from "react-bootstrap";
+import moment from "moment";
+import axios from "axios";
+import { SocketContext } from "../services/socket";
+import { socket } from "../services/socket";
 
 export default function NavBar() {
-  const { history, location, match } = useReactRouter()
-  const [userData, setUserData] = useState({})
-  const [messageData, setmessageData] = useState(0)
-
-
+  const { history, location, match } = useReactRouter();
+  const [userData, setUserData] = useState({});
+  const [messageData, setmessageData] = useState(0);
 
   useEffect(() => {
-    const ADMIN = localStorage.getItem(USER_KEY)
-    const _localJson = JSON.parse(ADMIN)
-    setUserData(_localJson)
-    if (!ADMIN) {
-      history.push(`/`)
-    }
-  }, [])
+    const getData = () => {
+      const ADMIN =  localStorage.getItem(USER_KEY);
+      const _localJson = JSON.parse(ADMIN);
+      setUserData(_localJson);
+      if (!ADMIN) {
+        history.push(`/`);
+      }
+    };
+    getData();
+  }, []);
 
   const _onLogout = () => {
-    localStorage.clear()
-    sessionStorage.clear()
-    history.push(`/`)
-  }
+    localStorage.clear();
+    sessionStorage.clear();
+    history.push(`/`);
+  };
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   socket.on(`MESSAGE_STORE:${userData?.data?.storeId}`, (data) => {
-    setmessageData(data)
+    setmessageData(data);
   });
-  
+
   return (
     <div>
       <Navbar
@@ -60,8 +60,7 @@ export default function NavBar() {
         }}
         variant="dark"
       >
-        <Navbar.Brand style={{ color: "#909090" }} href="#">
-        </Navbar.Brand>
+        <Navbar.Brand style={{ color: "#909090" }} href="#"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" />
@@ -73,10 +72,10 @@ export default function NavBar() {
             height={35}
             roundedCircle
             onClick={() => history.push("/messagerList")}
-            style={{ cursor: 'pointer'}}
-          // onClick={handleShow}
+            style={{ cursor: "pointer" }}
+            // onClick={handleShow}
           />
-          <Badge variant="danger" >{messageData ?? 0}</Badge>
+          <Badge variant="danger">{messageData ?? 0}</Badge>
           <div style={{ marginLeft: 30 }}></div>
           <Form inline>
             <Dropdown>
@@ -86,9 +85,11 @@ export default function NavBar() {
                 id="dropdown-basic"
               >
                 {userData
-                  ? (userData?.data?.firstname ? userData?.data?.firstname : "") +
-                  " " +
-                  (userData?.data?.lastname ? userData?.data?.lastname : "")
+                  ? (userData?.data?.firstname
+                      ? userData?.data?.firstname
+                      : "") +
+                    " " +
+                    (userData?.data?.lastname ? userData?.data?.lastname : "")
                   : ""}
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -101,7 +102,11 @@ export default function NavBar() {
               </Dropdown.Menu>
             </Dropdown>
             <Image
-              src={userData?.data?.image ? URL_PHOTO_AW3 + userData?.data?.image : ImageProfile}
+              src={
+                userData?.data?.image
+                  ? URL_PHOTO_AW3 + userData?.data?.image
+                  : ImageProfile
+              }
               width={45}
               height={45}
               roundedCircle
@@ -110,5 +115,5 @@ export default function NavBar() {
         </Navbar.Collapse>
       </Navbar>
     </div>
-  )
+  );
 }
