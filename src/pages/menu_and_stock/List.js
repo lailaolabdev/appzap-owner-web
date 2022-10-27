800afc5cc9c2ce426b41f64e5124c39a8d0533f0
phoudthavisack lsp <phoudthavisack@BOs-MacBook-Pro-2.local>
2022-10-27 12:07:16 +0700

    new_stock1

---
+++ src/pages/menu_and_stock/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PopUpAddMenu from "../../components/popup/PopUpAddMenu";
+
+export default function List() {
+  return (
+    <div>
+      <p>test</p>
+      <PopUpAddMenu open/>
+    </div>
+  );
+}
