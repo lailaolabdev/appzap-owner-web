c2699c410e6b0a7306eaf311da2e5b5134a01d85
Phoudthavisack lsp <phoudthavisack@gmail.com>
2023-07-19 20:55:35 +0700

    sort

--- src/routes/Menu.routes.js
+++ src/routes/Menu.routes.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 // page
 import MenuList from "../pages/menu_and_stock/List";
+import MenuSortPage from "../pages/menu/MenuSortPage";
 
 // eslint-disable-next-line
 export default {
   path: "/menu-new",
-  element: <MenuList />,
+  element: <Outlet />,
+  children: [
+    {
+      path: "",
+      element: <MenuList />,
+    },
+    {
+      path: "sort",
+      element: <MenuSortPage />,
+    },
+  ],
 };
