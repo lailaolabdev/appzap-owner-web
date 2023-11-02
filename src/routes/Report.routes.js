df20bd43434939ff8f32fbc19fde871b3d075578
Phoudthavisack lsp <phoudthavisack@gmail.com>
2023-11-02 15:06:34 +0700

    ok

--- src/routes/Report.routes.js
+++ src/routes/Report.routes.js
@@ -3,20 +3,21 @@ import { Outlet } from "react-router-dom";
 // page
 import SettingTable from "../pages/settingStore/SettingTable";
 import ReportMenuPage from "../pages/report/ReportMenuPage";
-import DashboardPage from "../pages/dashboardnew/DashboardPage"
+import DashboardPage from "../pages/dashboardnew/DashboardPage";
+import ReportStockPage from "../pages/report/ReportStockPage";
 
 // eslint-disable-next-line
 export default {
   path: "/new-report",
   element: <Outlet />,
   children: [
-     {
+    {
       path: "",
       element: <DashboardPage />,
     },
-    // {
-    //   path: "",
-    //   element: <ReportMenuPage />,
-    // },
+    {
+      path: "stock",
+      element: <ReportStockPage />,
+    },
   ],
 };
