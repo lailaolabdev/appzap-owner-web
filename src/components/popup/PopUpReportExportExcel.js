1d78f383fa4ca4772ce8fd6b289cd5619362ae59
phanukone <phanukonelvk@gmail.com>
2024-09-21 20:29:37 +0700

    code update export report

--- src/components/popup/PopUpReportExportExcel.js
+++ src/components/popup/PopUpReportExportExcel.js
@@ -17,6 +17,43 @@ export default function PopUpReportExportExcel({ open, onClose, setPopup }) {
   const { storeDetail, setStoreDetail } = useStore();
   const { t } = useTranslation();
 
+  const downloadExcel = async () => {
+    setPopup({ ReportExport: false });
+    try {
+      // findBy += `startDate=${storeDetail?.startDayFilter}&`;
+      // findBy += `endDate=${storeDetail?.endDayFilter}&`;
+      // findBy += `startTime=${storeDetail?.startTimeFilter}&`;
+      // findBy += `endTime=${storeDetail?.endTimeFilter}`;
+      const findBy = `&dateFrom=${storeDetail?.startDayFilter}&dateTo=${storeDetail?.endDayFilter}&timeTo=${storeDetail?.endTimeFilter}&timeFrom=${storeDetail?.startTimeFilter}`;
+      // setLoadingExportCsv(true);
+      const url =
+        END_POINT_EXPORT + "/export/bill?storeId=" + storeDetail?._id + findBy;
+      const _res = await Axios.get(url);
+
+      console.log("_res: " + _res?.data?.exportUrl);
+
+      if (_res?.data?.exportUrl) {
+        const response = await Axios.get(_res?.data?.exportUrl, {
+          responseType: "blob", // Important to get the response as a Blob
+        });
+
+        // Create a Blob from the response data
+        console.log("response", response.data);
+        const fileBlob = new Blob([response.data], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+
+        // Use the file-saver library to save the file with a new name
+        saveAs(fileBlob, storeDetail?.name + ".xlsx" || "export.xlsx");
+      }
+
+      // setLoadingExportCsv(false);
+    } catch (err) {
+      // setLoadingExportCsv(false);
+      errorAdd(`${t("export_fail")}`);
+    }
+  };
+
   const Promotions = async () => {
     setPopup({ ReportExport: false });
     try {
@@ -299,6 +336,12 @@ export default function PopUpReportExportExcel({ open, onClose, setPopup }) {
             disabled
           >
             <span>{t("sales_info")}</span>
+          </Button>
+          <Button
+            style={{ height: 100, padding: 20, width: 200 }}
+            onClick={downloadExcel}
+          >
+            <span>{t("excel")}</span>
           </Button>
           <Button
             style={{ height: 100, padding: 20, width: 200 }}
