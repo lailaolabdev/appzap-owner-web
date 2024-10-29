68b7b718191f69277301444b306cbcd08b878941
bounmy <bounmyvanhthongpheng09@gmail.com>
2024-10-29 17:28:42 +0700

    update code

--- src/pages/order/OrderPage.js
+++ src/pages/order/OrderPage.js
@@ -181,27 +181,9 @@ export default function OrderPage() {
       return false;
     }
   };
-  function wrapText(context, text, x, y, maxWidth, lineHeight) {
-    const words = text.split(" ");
-    let line = "";
-    for (let n = 0; n < words.length; n++) {
-      const testLine = line + words[n] + " ";
-      const metrics = context.measureText(testLine);
-      const testWidth = metrics.width;
-      if (testWidth > maxWidth && n > 0) {
-        context.fillText(line, x, y);
-        line = words[n] + " ";
-        y += lineHeight;
-      } else {
-        line = testLine;
-      }
-    }
-    context.fillText(line, x, y);
-  }
 
   const convertHtmlToBase64 = (orderSelect) => {
     const base64ArrayAndPrinter = [];
-
     orderSelect.forEach((data, index) => {
       if (data) {
         const canvas = document.createElement("canvas");
