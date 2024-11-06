ea0bb9b3fbbb404f2be27ef3c30f853aa861e598
phanukone <phanukonelvk@gmail.com>
2024-11-06 16:02:12 +0700

    code update

--- src/pages/table/components/CheckPopupDebt.js
+++ src/pages/table/components/CheckPopupDebt.js
@@ -551,7 +551,7 @@ export default function CheckPopupDebt({
   //   };
   // });
 
-  const optionsData = membersData.map((data) => {
+  const optionsData = membersData?.map((data) => {
     return {
       id: data?._id,
       value: data?.name,
