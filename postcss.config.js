dd47b6d7f14ecf7e92a119727aa976e0585a795a
tyecode <sengphachanh.dev@gmail.com>
2024-11-28 11:38:30 +0700

    fix: build issues

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,9 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: {}, // This handles CSS minimization
+    "postcss-preset-env": {
+      stage: 0, // Enable all modern CSS features
+    },
   },
 };
