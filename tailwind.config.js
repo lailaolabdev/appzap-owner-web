d82962044bea726f2e60b6d8597b051888ee199c
tyecode <sengphachanh.dev@gmail.com>
2024-12-05 10:26:39 +0700

    feat: update button styles and add cancel button in OrderPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       boxShadow: {
         button:
           "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;",
+        card: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.1)",
       },
       colors: {
         "color-app": "#FF6700",
