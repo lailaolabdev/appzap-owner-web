c6d9b41a0dc2aba0beb7fa8ed1a596aef8f61210
lailaohub <lailaolab@gmail.com>
2024-12-20 00:15:30 +0700

    add zustand first function

---
+++ src/zustand/orderStore.js
@@ -0,0 +1,34 @@
+import create from "zustand";
+
+export const useOrderStore = create((set) => ({
+  // State
+  orderItems: [],
+  orderLoading: false,
+
+  // Actions
+  setOrderItems: (items) => set({ orderItems: items }),
+
+  handleNewOrderItems: (newOrders) =>
+    set((state) => {
+      const updatedOrders = [...state.orderItems];
+      newOrders.forEach((newOrder) => {
+        const existingOrderIndex = updatedOrders.findIndex(
+          (order) => order._id === newOrder._id
+        );
+
+        if (existingOrderIndex > -1) {
+          // Update existing order
+          updatedOrders[existingOrderIndex] = {
+            ...updatedOrders[existingOrderIndex],
+            ...newOrder,
+          };
+        } else {
+          // Add new order
+          updatedOrders.push(newOrder);
+        }
+      });
+      return { orderItems: updatedOrders };
+    }),
+
+  setOrderLoading: (loading) => set({ orderLoading: loading }),
+}));
