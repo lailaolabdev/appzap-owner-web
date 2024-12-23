982b5754218f048e878ccdebd6fed5e3e4dedc3a
lailaohub <lailaolab@gmail.com>
2024-12-23 18:49:30 +0700

    persist order & sorting

--- src/zustand/orderStore.js
+++ src/zustand/orderStore.js
@@ -1,34 +1,45 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useOrderStore = create((set) => ({
-  // State
-  orderItems: [],
-  orderLoading: false,
+export const useOrderStore = create(
+  persist(
+    (set) => ({
+      // State
+      orderItems: [],
+      orderLoading: false,
 
-  // Actions
-  setOrderItems: (items) => set({ orderItems: items }),
+      // Actions
+      setOrderItems: (items) => set({ orderItems: items }),
 
-  handleNewOrderItems: (newOrders) =>
-    set((state) => {
-      const updatedOrders = [...state.orderItems];
-      newOrders.forEach((newOrder) => {
-        const existingOrderIndex = updatedOrders.findIndex(
-          (order) => order._id === newOrder._id
-        );
+      handleNewOrderItems: (newOrders) =>
+        set((state) => {
+          const updatedOrders = [...state.orderItems]; // Copy the current orderItems
 
-        if (existingOrderIndex > -1) {
-          // Update existing order
-          updatedOrders[existingOrderIndex] = {
-            ...updatedOrders[existingOrderIndex],
-            ...newOrder,
-          };
-        } else {
-          // Add new order
-          updatedOrders.push(newOrder);
-        }
-      });
-      return { orderItems: updatedOrders };
+          newOrders.forEach((newOrder) => {
+            const existingOrderIndex = updatedOrders.findIndex(
+              (order) => order._id === newOrder._id
+            );
+
+            if (existingOrderIndex > -1) {
+              // Update the existing order at the front of the list
+              updatedOrders[existingOrderIndex] = {
+                ...updatedOrders[existingOrderIndex],
+                ...newOrder,
+              };
+            } else {
+              // Add the new order at the front
+              updatedOrders.unshift(newOrder);
+            }
+          });
+
+          return { orderItems: updatedOrders };
+        }),
+
+      setOrderLoading: (loading) => set({ orderLoading: loading }),
     }),
-
-  setOrderLoading: (loading) => set({ orderLoading: loading }),
-}));
+    {
+      name: "orderStore", // Name of the storage key in localStorage
+      getStorage: () => localStorage, // Use localStorage as the storage method
+    }
+  )
+);
