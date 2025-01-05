2882eaa6cc1d40c6d41bd152cf16e8f14d6899b7
lailaohub <lailaolab@gmail.com>
2025-01-05 22:08:09 +0700

    remove menu & menuCategory globalState file

--- src/store/StateProvider.js
+++ src/store/StateProvider.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Context } from "./Context";
 import { useTableState } from "./globalState/tableState";
 import { useOrderState } from "./globalState/orderState";
-// import { useMenuCategoryState } from "./globalState/menuCategoryState";
-// import { useMenuState } from "./globalState/menuState";
 import { useReservationState } from "./globalState/reservationState";
 import { usePrintersState } from "./globalState/printerState";
 import { useSoundState } from "./globalState/soundState";
@@ -21,20 +19,15 @@ export const StateProvider = ({ children }) => {
   const splitBill = useBillState();
   const order = useOrderState();
   const table = useTableState(storeDetail || {});
-  // const menuCategory = useMenuCategoryState(storeDetail || {});
   const reservation = useReservationState();
   const printer = usePrintersState();
   const sound = useSoundState();
   const socket = useSocketState({...sound });
   const user = UserState();
-  // const menu = useMenuState(storeDetail || {});
   const themeState = useThemeState();
   let store = Object.assign(
     order,
     table,
-    // storeDetail,
-    // menuCategory,
-    // menu,
     reservation,
     printer,
     sound,
