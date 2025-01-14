cfe019fc66ce4910fef7508692bac52c74fe91ad
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-01-14 13:25:43 +0700

    use exceljs

--- src/components/popup/PopUpDebtExport.js
+++ src/components/popup/PopUpDebtExport.js
@@ -4,6 +4,8 @@ import { Button, Form, Card, Spinner, Modal } from "react-bootstrap";
 import { getLocalData } from "../../constants/api";
 import { getBilldebts, getdebtHistory } from "../../services/debt";
 import moment from "moment";
+import ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
 import { moneyCurrency } from "../../helpers";
 import ImageEmpty from "../../image/empty.png";
 import { MdOutlineCloudDownload } from "react-icons/md";
@@ -48,6 +50,63 @@ export default function PopUpDebtExport({
   //   }
   // };
 
+  const exportToExcel = async () => {
+    // create workbook and worksheet
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('รายงานหนี้');
+
+    // add header
+    worksheet.mergeCells('A1:G1');
+    const titleCell = worksheet.getCell('A1');
+    titleCell.value = 'ລາຍງານຂໍ້ມູນການຕິດຫນີ້';
+    titleCell.font = { size: 16, bold: true };
+    titleCell.alignment = { horizontal: 'center' };
+
+    
+    // add table header
+    worksheet.addRow([`${t("#")}`, `${t("bill_no")}`, `${t("name")}`,`${t("tel")}`,`${t("money_remaining")}`, `${t("debt_pay_remaining")}`, `${t("payment_datetime_debt")}`]);
+    
+    // layout style
+    const headerRow = worksheet.getRow(3);
+    headerRow.font = { bold: true };
+    headerRow.alignment = { horizontal: 'center' };
+
+    // add data
+    const filteredData = billDebtData
+      .filter(e => e?.remainingAmount > 0)
+      .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+
+    filteredData.forEach((item, index) => {
+      worksheet.addRow([
+        index + 1,
+        item.code,
+        item.customerName,
+        item.customerPhone,
+        item.remainingAmount,
+        item.totalPayment,
+        item.outStockDate ? moment(item.outStockDate).format("DD/MM/YYYY - HH:mm:SS") : ''
+      ]);
+    });
+
+    // layout money money
+    worksheet.getColumn(3).numFmt = '#,##0.00';
+    worksheet.getColumn(4).numFmt = '#,##0.00';
+
+    // size column
+    worksheet.columns.forEach(column => {
+      column.width = 20;
+    });
+
+    // create file
+    const buffer = await workbook.xlsx.writeBuffer();
+    const fileDate = moment().format('YYYYMMDD_HHmmss');
+    saveAs(
+      new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }),
+      `ລາຍງານຫນີ້${fileDate}.xlsx`
+    );
+  };
+
+
   useEffect(() => {
     if (open) {
       //getDataHistory();
@@ -59,18 +118,20 @@ export default function PopUpDebtExport({
       ...debtData,
     });
     setShowUserModal(true);
-    setShowMainModal(false); // ซ่อน PopUpDebtExport
+    setShowMainModal(false); 
   };
 
   const handleUserModalClose = () => {
     setShowUserModal(false);
-    setShowMainModal(true); // แสดง PopUpDebtExport อีกครั้ง
+    setShowMainModal(true); 
   };
 
   const handleMainModalClose = () => {
     setShowMainModal(false);
     onClose();
   };
+
+  console.log("billDebtData: ",billDebtData)
 
   return (
     <>
@@ -93,6 +154,7 @@ export default function PopUpDebtExport({
                 {t("customer_name")}: {billDebtData?.customerName}
               </span>
               <Button
+                onClick={exportToExcel}
                 style={{
                   marginLeft: 'auto',
                   color: "white",
@@ -120,9 +182,11 @@ export default function PopUpDebtExport({
               <thead>
                 <tr>
                   <th style={{ paddingRight: "3rem" }}>#</th>
-                  <th style={{ paddingRight: "10rem" }}>{t("bill_no")}</th>
-                  <th style={{ paddingRight: "5rem" }}>{t("money_remaining")}</th>
-                  <th style={{ paddingRight: "5rem" }}>{t("debt_pay_remaining")}</th>
+                  <th style={{ paddingRight: "4rem" }}>{t("bill_no")}</th>
+                  <th style={{ paddingRight: "4rem" }}>{t("name")}</th>
+                  <th style={{ paddingRight: "7rem" }}>{t("tel")}</th>
+                  <th style={{ paddingRight: "2rem" }}>{t("money_remaining")}</th>
+                  <th style={{ paddingRight: "2rem" }}>{t("debt_pay_remaining")}</th>
                   <th>{t("payment_datetime_debt")}</th>
                 </tr>
               </thead>
@@ -145,7 +209,9 @@ export default function PopUpDebtExport({
                       >
                         <td>{(pagination - 1) * limitData + i + 1}</td>
                         <td>{e.code}</td>
-                        <td>{moneyCurrency(e?.remainingAmount)}</td>
+                        <td>{(e?.customerName)}</td>
+                        <td>{(e?.customerPhone)}</td>
+                        <td>{moneyCurrency(e?.remainingAmount)}</td>                      
                         <td style={{ color: "MediumSeaGreen" }}>{moneyCurrency(e?.totalPayment)}</td>
                         <td>
                           {e?.outStockDate
