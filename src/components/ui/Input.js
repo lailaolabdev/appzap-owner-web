7edaec4d4ebf2903790b07c72a7f4617019165c1
phanukone <lailaolabdev@gmail.com>
2025-05-03 17:40:31 +0700

    feat: add discount popup with new UI components and total calculation

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -1,16 +1,13 @@
 import * as React from "react";
 
-import { cn } from "../../utils/cn";
+import { cn } from "../../lib/utils";
 
-const Input = React.forwardRef(function Input(
-  { className, type, ...props },
-  ref
-) {
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
     <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#FB6E3B] focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
       ref={ref}
@@ -18,6 +15,7 @@ const Input = React.forwardRef(function Input(
     />
   );
 });
+
 Input.displayName = "Input";
 
 export { Input };
