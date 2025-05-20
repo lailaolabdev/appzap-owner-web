6cadb844d64777fcc0d4b48b94c58af3347c398a
bounmy <lailaolabdev@gmail.com>
2025-05-20 14:38:36 +0700

    done
    (cherry picked from commit 842de48500452bd1d5c16ce6317cf1ed2200e811)

--- src/components/popup/PopUpManageCounter.js
+++ src/components/popup/PopUpManageCounter.js
@@ -86,6 +86,9 @@ export default function PopUpManageCounter({
   const minDate = moment()
     .subtract(days - 1, "days")
     .format("YYYY-MM-DD");
+  const minDate = moment()
+    .subtract(days - 1, "days")
+    .format("YYYY-MM-DD");
   const maxDate = moment().format("YYYY-MM-DD");
 
   return (
