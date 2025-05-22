82abbbfbd61a66f5820454e17599e7cfff18b846
phanukone <lailaolabdev@gmail.com>
2025-05-22 10:29:23 +0700

    code push

--- src/pages/dashboard/DashboardFinance.js
+++ src/pages/dashboard/DashboardFinance.js
@@ -504,7 +504,8 @@ export default function DashboardFinance({
 
   const baseTotal =
     (dataModal?.point ?? 0) +
-    (dataModal?.transferAmount || (dataModal?.billAmount ?? 0)) +
+    (dataModal?.transferAmount ?? 0) +
+    (dataModal?.billAmount ?? 0) +
     (dataModal?.change ?? 0) +
     (dataModal?.payAmount ?? 0) -
     (dataModal?.discount ?? 0) -
@@ -518,6 +519,9 @@ export default function DashboardFinance({
         (dataModal?.taxAmount ?? 0) +
         (dataModal?.serviceChargeAmount ?? 0)
       : baseTotal - dataModal?.change;
+
+  console.log("totalAfter", totalAfter);
+  console.log("dataModal", dataModal);
 
   let TotalCalculate = 0;
 
@@ -567,7 +571,6 @@ export default function DashboardFinance({
         createdBy: item?.createdBy?.firstname || "-",
         isWeightMenu: item?.isWeightMenu,
         unitWeightMenu: item?.unitWeightMenu,
-        price: item?.price || 0,
         totalPrice: (() => {
           if (isCanceled) return "CANCELED";
           try {
@@ -625,7 +628,7 @@ export default function DashboardFinance({
   const calculateTotalAmount = (orderData) => {
     try {
       return orderData.reduce((total, item) => {
-        const price = item.price || 0;
+        const price = parseFloat(item.totalPrice.replace(/,/g, "")) || 0;
         return total + price;
       }, 0);
     } catch {
@@ -1154,7 +1157,7 @@ export default function DashboardFinance({
                   </td>
                   <td>{item.createdBy}</td>
 
-                  <td>{item?.price || 0}</td>
+                  <td>{calculateDiscount(item)}</td>
 
                   {storeDetail?.isDelivery && (
                     <td style={{ textAlign: "center" }}>{item.deliveryCode}</td>
