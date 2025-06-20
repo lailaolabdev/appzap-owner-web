ef9752eb3ae690f71cbec28d57ca3df2626e8cab
macbook <lailaolabdev@gmail.com>
2025-06-20 14:30:42 +0700

    code push

--- src/zustand/counterRole.js
+++ src/zustand/counterRole.js
@@ -4,8 +4,8 @@ import { persist } from "zustand/middleware";
 export const useCounterRoleStore = create(
   persist(
     (set) => ({
-      counterRoleEditBill: false,
-      counterRoleEditMenu: false,
+      counterRoleEditBill: true,
+      counterRoleEditMenu: true,
       setCounterRoleEditBill: (counterRole) =>
         set({ counterRoleEditBill: counterRole }),
       setCounterRoleEditMenu: (counterRole) =>
